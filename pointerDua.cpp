72d4290074b3e07b6152939f75d92c45a9763132
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-27 15:41:49 +0700

    int main

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -12,3 +12,15 @@ public:
     }
 };
 
+int main()
+{
+    // Membuat objek mahasiswa secara dinamis di memory (heap)
+    mahasiswa *mhs = new mahasiswa{1}; // pointer Object mhs
+    
+    mhs->nim = 2;        // Mengubah nilai nim menggunakan operator panah
+    mhs->showNim();      // Memanggil method menggunakan operator panah
+    
+    delete mhs;          // Menghapus objek dari memori (penting untuk mencegah memory leak)
+    
+    return 0;
+}
