6e482dc7d549d214d9782cfccaf9d24ae7f3b706
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-27 15:24:43 +0700

    menambahkan class siswa danorang

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+class siswa; // Forward declaration
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama) {
+        nama = pNama;
+    }
+    friend class siswa;
+};
+
